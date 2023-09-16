d4b3fb2b2b4b0266df14417c10d8eb1bf1a96fd4
Robinson Alvarez <robinnet28@hotmail.com>
2023-09-16 01:06:56 -0300

    more fixes

--- src/main/java/com/example/postsapi/PostsApiApplication.java
+++ src/main/java/com/example/postsapi/PostsApiApplication.java
@@ -7,13 +7,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.postsapi.adapter.out")
 public class PostsApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PostsApiApplication.class, args);
+        SpringApplication app = new SpringApplication(PostsApiApplication.class);
+        String port = System.getenv("PORT");
+        app.setDefaultProperties(Collections.singletonMap("server.port", port == null ? "8080" : port));
+        app.run(args);
     }
 
     @Bean
